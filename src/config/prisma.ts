52c1798601c616632653de9aa983578c3f7819a2
Symbosys <symbosystech@gmail.com>
2026-01-30 11:57:26 +0530

    last changes

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -2,13 +2,22 @@ import "dotenv/config";
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '../../generated/prisma/client';
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST === 'localhost' ? '127.0.0.1' : process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 5
+  port: Number(process.env.DATABASE_PORT) || 3306,
+  connectionLimit: 20
 });
-const prisma: PrismaClient = new PrismaClient({ adapter });
 
-export { prisma }
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ['error', 'warn'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
